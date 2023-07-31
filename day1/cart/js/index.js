c39c5e1a02a2db973713ac3be9d086431638dc0e
xxxxx-3747 <1921708164@qq.com>
2023-07-31 21:46:10 +0800

    第四屏动画需修改版

--- day1/cart/js/index.js
+++ day1/cart/js/index.js
@@ -4,19 +4,33 @@ $(function () {
         verticalCentered: false,
         navigation: true,
         afterLoad:function (link,index) {
-            console.log(index.index);
+            // console.log(index.index);
             /*index 序号 1开始  当前屏的序号*/
             $('.section').eq(index.index).addClass('now');
         },
         onLeave:function (index,nextIndex,direction ) {
-            if(index == 2 && nextIndex == 3){
+            console.log(index.index);
+            console.log(nextIndex.index);
+            if(index.index == 1  && nextIndex.index == 2){
                 $('.section').eq(index.index).addClass('leaved');
-            }else if(index == 3 && nextIndex == 4){
+            }else if(index.index == 2 && nextIndex.index == 3){
                 $('.section').eq(index.index).addClass('leaved');
             }
 
 
-        }
+        },
+        afterRender:function () {
+            //点击更多向下滚动
+            $(".more").on('click',function () {
+                $.fn.fullpage.moveSectionDown();
+            });
+            $(".screen04 .content .cart").on('transitionend',function () {
+                console.log("xxx");
+                $(".screen04 .content .address").addClass("show");
+
+            })
+        },
+        scrollingSpeed:1000
     });
 });
 
