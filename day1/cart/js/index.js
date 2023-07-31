ab6e20384f15a0e92ca70227f335080ca064c26e
xxxxx-3747 <1921708164@qq.com>
2023-07-31 13:14:05 +0800

    第三屏

--- day1/cart/js/index.js
+++ day1/cart/js/index.js
@@ -1,12 +1,21 @@
 $(function () {
-    $('#dowebok').fullpage({
+    $('.dowebok').fullpage({
         sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
         verticalCentered: false,
         navigation: true,
-        afterLoad:function (Link,index) {
-            // console.log($('.section').index);
-            console.log($('.section').eq(index-1));
-            $('.dowebok').eq(index-1).addClass('now');
+        afterLoad:function (link,index) {
+            console.log(index.index);
+            /*index 序号 1开始  当前屏的序号*/
+            $('.section').eq(index.index).addClass('now');
+        },
+        onLeave:function (index,nextIndex,direction ) {
+            if(index == 2 && nextIndex == 3){
+                $('.section').eq(index.index).addClass('leaved');
+            }else if(index == 3 && nextIndex == 4){
+                $('.section').eq(index.index).addClass('leaved');
+            }
+
+
         }
     });
 });
